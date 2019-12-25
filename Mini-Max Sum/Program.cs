6fee5e985af14ada1fc60f219f99b713feeeb403
Zain Arif <zainarif14197@gmail.com>
2019-12-25 13:35:51 +0500

    code

---
+++ Mini-Max Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Max_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 5 numbers : ");
+            int[] arr = new int[5];
+            for (int x = 0; x < 5; x++)
+                arr[x] = int.Parse(Console.ReadLine());
+            miniMaxSum(arr);
+        }
+
+        static void miniMaxSum(int[] arr)
+        {
+            Array.Sort(arr);
+            Int64 min = 0, max = 0;
+            for (int i = 0; i < 4; i++)
+                min += arr[i];
+
+            for (int i = 1; i <= 4; i++)
+                max += arr[i];
+
+            Console.WriteLine("{0} {1}", min, max);
+            Console.ReadLine();
+        }
+    }
+}
